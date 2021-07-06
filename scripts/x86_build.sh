e27634d9a006051cb5931525ae6d99b75c418c2b
binhu01 <bin.hu@armchina.com>
2021-07-06 18:20:24 +0800

    add the Deeplab v3 demo initially

---
+++ scripts/x86_build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+CURRENT_PATH=$(readlink -f "$(dirname "$0")")
+BASEDIR=$CURRENT_PATH/../..
+
+echo $CURRENT_PATH
+echo $BASEDIR
+
+cmake .. \
+    -DBOOST_ROOT=$BASEDIR/boost_x86_install/ \
+    -DBUILD_TF_LITE_PARSER=1 \
+	-DTF_LITE_GENERATED_PATH=$BASEDIR/tflite \
+	-DFLATBUFFERS_ROOT=$BASEDIR/flatbuffers \
+	-DARMNNREF=1 \
+    -DBUILD_TESTS=1 \
+	-DBUILD_SAMPLE_APP=1 
+
+
+make -j12
