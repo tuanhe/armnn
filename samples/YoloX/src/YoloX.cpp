f73c8254ba97d444a2ce94db9596ba1fec5be144
binhu01 <bin.hu@armchina.com>
2021-08-25 13:14:09 +0800

    YoloX demo is ready

--- samples/YoloX/src/YoloX.cpp
+++ samples/YoloX/src/YoloX.cpp
@@ -18,24 +18,7 @@
 #include <armnnTfLiteParser/ITfLiteParser.hpp>
 #include <armnnOnnxParser/IOnnxParser.hpp>
 
-#define CV 1
-
-#if CV
-#include "opencv2/opencv.hpp"
-
-//using namespace cv;
-
-#else
-//image process
-#define STB_IMAGE_IMPLEMENTATION
-#include <stb/stb_image.h>
-
-#define STB_IMAGE_RESIZE_IMPLEMENTATION
-#include <stb/stb_image_resize.h>
-
-#define STB_IMAGE_WRITE_IMPLEMENTATION
-#include <stb/stb_image_write.h>
-#endif
+#include "YoloX.hpp"
 
 // Application parameters
 std::vector<armnn::BackendId> default_preferred_backends_order = {armnn::Compute::CpuRef};
@@ -45,9 +28,13 @@ std::string preferred_backend_str;
 std::string input_file_str;
 int nb_loops = 1;
 
-uint32_t INPUT_W = 416;
-uint32_t INPUT_H = 416;
-uint32_t nClasses = 3;
+#define NMS_THRESH 0.45
+#define BBOX_CONF_THRESH 0.3
+
+static const int INPUT_W = 416;
+static const int INPUT_H = 416;
+static const int NUM_CLASSES = 80;
+
 float mean[3] = {0.485, 0.456, 0.406};
 float stdev[3] = {0.229, 0.224, 0.225};
 
@@ -155,6 +142,282 @@ void yolox_normalize(cv::Mat& img, void* data)
     }
 }
 
+
+static void generate_grids_and_stride(const int target_w, const int target_h, std::vector<int>& strides, std::vector<GridAndStride>& grid_strides)
+{
+    for (auto stride : strides)
+    {
+        int num_grid_w = target_w / stride;
+        int num_grid_h = target_h / stride;
+        for (int g1 = 0; g1 < num_grid_h; g1++)
+        {
+            for (int g0 = 0; g0 < num_grid_w; g0++)
+            {
+                grid_strides.push_back((GridAndStride){g0, g1, stride});
+            }
+        }
+    }
+}
+
+
+static void generate_yolox_proposals(std::vector<GridAndStride> grid_strides, float* feat_blob, float prob_threshold, std::vector<Object>& objects)
+{
+
+    const int num_anchors = grid_strides.size();
+
+    for (int anchor_idx = 0; anchor_idx < num_anchors; anchor_idx++)
+    {
+        const int grid0 = grid_strides[anchor_idx].grid0;
+        const int grid1 = grid_strides[anchor_idx].grid1;
+        const int stride = grid_strides[anchor_idx].stride;
+
+        const int basic_pos = anchor_idx * (NUM_CLASSES + 5);
+
+        // yolox/models/yolo_head.py decode logic
+        float x_center = (feat_blob[basic_pos+0] + grid0) * stride;
+        float y_center = (feat_blob[basic_pos+1] + grid1) * stride;
+        float w = exp(feat_blob[basic_pos+2]) * stride;
+        float h = exp(feat_blob[basic_pos+3]) * stride;
+        float x0 = x_center - w * 0.5f;
+        float y0 = y_center - h * 0.5f;
+
+        float box_objectness = feat_blob[basic_pos+4];
+        for (int class_idx = 0; class_idx < NUM_CLASSES; class_idx++)
+        {
+            float box_cls_score = feat_blob[basic_pos + 5 + class_idx];
+            float box_prob = box_objectness * box_cls_score;
+            if (box_prob > prob_threshold)
+            {
+                Object obj;
+                obj.rect.x = x0;
+                obj.rect.y = y0;
+                obj.rect.width = w;
+                obj.rect.height = h;
+                obj.label = class_idx;
+                obj.prob = box_prob;
+
+                objects.push_back(obj);
+            }
+
+        } // class loop
+
+    } // point anchor loop
+}
+
+static inline float intersection_area(const Object& a, const Object& b)
+{
+    cv::Rect_<float> inter = a.rect & b.rect;
+    return inter.area();
+}
+
+static void qsort_descent_inplace(std::vector<Object>& faceobjects, int left, int right)
+{
+    int i = left;
+    int j = right;
+    float p = faceobjects[(left + right) / 2].prob;
+
+    while (i <= j)
+    {
+        while (faceobjects[i].prob > p)
+            i++;
+
+        while (faceobjects[j].prob < p)
+            j--;
+
+        if (i <= j)
+        {
+            // swap
+            std::swap(faceobjects[i], faceobjects[j]);
+
+            i++;
+            j--;
+        }
+    }
+
+    #pragma omp parallel sections
+    {
+        #pragma omp section
+        {
+            if (left < j) qsort_descent_inplace(faceobjects, left, j);
+        }
+        #pragma omp section
+        {
+            if (i < right) qsort_descent_inplace(faceobjects, i, right);
+        }
+    }
+}
+
+static void qsort_descent_inplace(std::vector<Object>& objects)
+{
+    if (objects.empty())
+        return;
+
+    qsort_descent_inplace(objects, 0, objects.size() - 1);
+}
+
+
+static void nms_sorted_bboxes(const std::vector<Object>& faceobjects, std::vector<int>& picked, float nms_threshold)
+{
+    picked.clear();
+
+    const int n = faceobjects.size();
+
+    std::vector<float> areas(n);
+    for (int i = 0; i < n; i++)
+    {
+        areas[i] = faceobjects[i].rect.area();
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        const Object& a = faceobjects[i];
+
+        int keep = 1;
+        for (int j = 0; j < (int)picked.size(); j++)
+        {
+            const Object& b = faceobjects[picked[j]];
+
+            // intersection over union
+            float inter_area = intersection_area(a, b);
+            float union_area = areas[i] + areas[picked[j]] - inter_area;
+            // float IoU = inter_area / union_area
+            if (inter_area / union_area > nms_threshold)
+                keep = 0;
+        }
+
+        if (keep)
+            picked.push_back(i);
+    }
+}
+
+static void decode_outputs(float* prob, std::vector<Object>& objects, float scale, const int img_w, const int img_h) 
+{
+    std::vector<Object> proposals;
+    std::vector<int> strides = {8, 16, 32};
+    std::vector<GridAndStride> grid_strides;
+    generate_grids_and_stride(INPUT_W, INPUT_H, strides, grid_strides);
+    generate_yolox_proposals(grid_strides, prob,  BBOX_CONF_THRESH, proposals);
+    std::cout << "num of boxes before nms: " << proposals.size() << std::endl;
+
+    qsort_descent_inplace(proposals);
+
+    std::vector<int> picked;
+    nms_sorted_bboxes(proposals, picked, NMS_THRESH);
+
+
+    int count = picked.size();
+
+    std::cout << "num of boxes: " << count << std::endl;
+
+    objects.resize(count);
+    for (int i = 0; i < count; i++)
+    {
+        objects[i] = proposals[picked[i]];
+
+        // adjust offset to original unpadded
+        float x0 = (objects[i].rect.x) / scale;
+        float y0 = (objects[i].rect.y) / scale;
+        float x1 = (objects[i].rect.x + objects[i].rect.width) / scale;
+        float y1 = (objects[i].rect.y + objects[i].rect.height) / scale;
+
+        // clip
+        x0 = std::max(std::min(x0, (float)(img_w - 1)), 0.f);
+        y0 = std::max(std::min(y0, (float)(img_h - 1)), 0.f);
+        x1 = std::max(std::min(x1, (float)(img_w - 1)), 0.f);
+        y1 = std::max(std::min(y1, (float)(img_h - 1)), 0.f);
+
+        objects[i].rect.x = x0;
+        objects[i].rect.y = y0;
+        objects[i].rect.width = x1 - x0;
+        objects[i].rect.height = y1 - y0;
+    }
+}
+
+
+static int image_pre_process(const char* file, armnn::TensorInfo& tensorInfo, void* data)
+{
+    cv::Mat resized_img = static_resize(file);
+    yolox_normalize(resized_img, data);
+    return 0;
+}
+
+static void draw_objects(const cv::Mat& bgr, const std::vector<Object>& objects)
+{
+    static const char* class_names[] = {
+        "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat", "traffic light",
+        "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow",
+        "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee",
+        "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard",
+        "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+        "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "couch",
+        "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse", "remote", "keyboard", "cell phone",
+        "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear",
+        "hair drier", "toothbrush"
+    };
+
+    cv::Mat image = bgr.clone();
+
+    for (size_t i = 0; i < objects.size(); i++)
+    {
+        const Object& obj = objects[i];
+
+        fprintf(stderr, "%d = %.5f at %.2f %.2f %.2f x %.2f\n", obj.label, obj.prob,
+                obj.rect.x, obj.rect.y, obj.rect.width, obj.rect.height);
+
+        cv::Scalar color = cv::Scalar(color_list[obj.label][0], color_list[obj.label][1], color_list[obj.label][2]);
+        float c_mean = cv::mean(color)[0];
+        cv::Scalar txt_color;
+        if (c_mean > 0.5){
+            txt_color = cv::Scalar(0, 0, 0);
+        }else{
+            txt_color = cv::Scalar(255, 255, 255);
+        }
+
+        cv::rectangle(image, obj.rect, color * 255, 2);
+
+        char text[256];
+        sprintf(text, "%s %.1f%%", class_names[obj.label], obj.prob * 100);
+
+        int baseLine = 0;
+        cv::Size label_size = cv::getTextSize(text, cv::FONT_HERSHEY_SIMPLEX, 0.4, 1, &baseLine);
+
+        cv::Scalar txt_bk_color = color * 0.7 * 255;
+
+        int x = obj.rect.x;
+        int y = obj.rect.y + 1;
+        //int y = obj.rect.y - label_size.height - baseLine;
+        if (y > image.rows)
+            y = image.rows;
+        //if (x + label_size.width > image.cols)
+            //x = image.cols - label_size.width;
+
+        cv::rectangle(image, cv::Rect(cv::Point(x, y), cv::Size(label_size.width, label_size.height + baseLine)),
+                      txt_bk_color, -1);
+
+        cv::putText(image, text, cv::Point(x, y + label_size.height),
+                    cv::FONT_HERSHEY_SIMPLEX, 0.4, txt_color, 1);
+    }
+
+    cv::imwrite("yolox_ed.jpg" , image);
+    fprintf(stderr, "save vis file\n");
+    /* cv::imshow("image", image); */
+    /* cv::waitKey(0); */
+}
+
+static int image_post_process(const char* file, void* data)
+{
+    std::vector<Object> objects;
+    float* output_buffer = reinterpret_cast<float*>(data);
+    cv::Mat image = cv::imread(file);
+    float scale = std::min(INPUT_W / (image.cols * 1.0), INPUT_H / (image.rows * 1.0));
+
+    decode_outputs(output_buffer, objects, scale,  image.cols,  image.rows);
+    //cv::imwrite("seg.jpg", result);
+    draw_objects(image, objects);
+
+    return 0;
+}
+
 static int read_bin_file(const char* file, void* inputData, uint32_t inputDataSize)
 {
     char * buffer;
@@ -167,43 +430,6 @@ static int read_bin_file(const char* file, void* inputData, uint32_t inputDataSi
     in.close();
     memcpy(inputData, buffer, size);
     delete[] buffer;
-
-    return 0;
-}
-
-static int image_pre_process(const char* file, armnn::TensorInfo& tensorInfo, void* data)
-{
-    cv::Mat resized_img = static_resize(file);
-    yolox_normalize(resized_img, data);
-    return 0;
-}
-
-static int image_post_process(const char* file, void* data)
-{
-	cv::Mat mask = cv::Mat(INPUT_W, INPUT_H, CV_8UC1, data);
-    cv::imwrite("debug0.jpg", mask);
-    cv::cvtColor(mask, mask, cv::COLOR_GRAY2RGB);
-    cv::imwrite("debug1.jpg", mask);
-    
-    cv::Mat srcTest = cv::imread(file);
-    int origWidth = srcTest.cols;
-    int origHeight = srcTest.rows;
-    std::cout << "origWidth" << origWidth << "\n";
-    std::cout << "origHeight" << origHeight << "\n";
-    cv::resize(mask, mask, cv::Size(origWidth, origHeight), 0, 0, cv::INTER_CUBIC);
-    cv::imwrite("debug2.jpg", mask);
-    cv::threshold(mask, mask, 128, 255, cv::THRESH_BINARY);
-    cv::imwrite("debug3.jpg", mask);
-    
-    //seems no useful
-    //cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(5, 5));
-    //cv::dilate(mask, mask, element);    
-    cv::Mat bgMask = ~mask;
-    cv::Mat result;
-    cv::GaussianBlur(bgMask, bgMask, cv::Size(), 10);
-    cv::add(srcTest, bgMask, result);
-    
-    cv::imwrite("seg.jpg", result);
 
     return 0;
 }
@@ -333,7 +559,7 @@ int main(int argc, char* argv[])
     
     image_pre_process(input_file_str.c_str(), inputTensorInfos[0], in[0].data());
     //read_bin_file(input_file_str.c_str(), in[0].data(), inputTensorInfos[0].GetNumElements());
-    write2buffer("input", in[0].data(), inputTensorInfos.at(0).GetNumBytes());
+    //write2buffer("input", in[0].data(), inputTensorInfos.at(0).GetNumBytes());
 
     // Run the inferences
     std::cout << "\ninferences are running: " << std::flush;
@@ -345,15 +571,14 @@ int main(int argc, char* argv[])
     }
     std::cout << "\n";
 
-    write2buffer("CpuRef", out[0].data(), outputTensorInfos.at(0).GetNumBytes());
+    //write2buffer("CpuRef", out[0].data(), outputTensorInfos.at(0).GetNumBytes());
+    //float* ptr = reinterpret_cast<float*>(out[0].data());
+    //for(uint32_t i = 0 ; i <  outputTensorInfos.at(0).GetNumBytes()/4; i++)
+    //{
+    //    printf("%e \n", ptr[i]);
+    //}
 
-    float* ptr = reinterpret_cast<float*>(out[0].data());
-    for(uint32_t i = 0 ; i <  outputTensorInfos.at(0).GetNumBytes()/4; i++)
-    {
-        printf("%e \n", ptr[i]);
-    }
-
-    //image_post_process(input_file_str.c_str(), out[0].data());
+    image_post_process(input_file_str.c_str(), out[0].data());
 
     return 0;
 }
